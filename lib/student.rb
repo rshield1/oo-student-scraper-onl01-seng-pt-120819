a69d142fd561d28343f0decc70937efd99bc54ea
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-23 03:18:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,10 @@ class Student
 
   def initialize(student_hash)
     
+    student_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
